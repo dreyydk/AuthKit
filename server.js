7d50f6775c2c0d5a9a2dbfafcf260b52e7368f9c
Andrei Bacin <andreib.pf@gmail.com>
2024-11-11 13:56:46 -0300

    chore: Server setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connect from "./src/db/connect.js";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
 
 dotenv.config();
 
@@ -11,9 +14,23 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const routeFiles = fs.readdirSync("./src/routes");
+
+routeFiles.forEach((file) => {
+  import(`./src/routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1", route.default);
+    })
+    .catch((error) => {
+      console.log("Failed to load route file: ", error);
+    });
+});
 
 const server = async () => {
   try {
+    await connect();
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });

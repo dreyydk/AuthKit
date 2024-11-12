288ae7f5d9735153e74ba15f9b888632d6b6206e
Andrei Bacin <andreib.pf@gmail.com>
2024-11-12 09:33:02 -0300

    fix: Password hash issue

--- src/models/auth/UserModel.js
+++ src/models/auth/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -40,6 +41,18 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true, minimize: false }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+
+  next();
+});
 
 const User = mongoose.model("User", UserSchema);
 

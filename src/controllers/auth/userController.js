bf73e9715cfe0edd211e357e7a11bfdc6e9e5b28
Andrei Bacin <andreib.pf@gmail.com>
2024-11-12 11:48:26 -0300

    feat: User login status

--- src/controllers/auth/userController.js
+++ src/controllers/auth/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import User from "../../models/auth/UserModel.js";
 import generateToken from "../../helpers/generateToken.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -124,5 +125,21 @@ export const updateUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404).json({ message: "User not found!" });
+  }
+});
+
+export const userLoginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, please login!" });
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (decoded) {
+    res.status(200).json(true);
+  } else {
+    res.status(401).json(false);
   }
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   logoutUser,
   registerUser,
   updateUser,
+  userLoginStatus,
 } from "../controllers/auth/userController.js";
 import {
   adminMiddleware,
@@ -20,11 +21,13 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/logout", logoutUser);
+router.post("/logout", logoutUser);
 router.get("/user", protect, getUser);
 router.patch("/user", protect, updateUser);
 
 router.delete("/admin/users/:id", protect, adminMiddleware, deleteUser);
 router.get("/admin/users", protect, creatorMiddleware, getAllUsers);
+
+router.get("/login-status", userLoginStatus);
 
 export default router;
